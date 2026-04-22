0e61b36b3933e20687107c231c466813abc0ba6a
Arturo Nochi <arturonochi@gmail.com>
2026-04-22 12:14:51 -0500

    unicorn cloudfare 2 workers

--- run_web.py
+++ run_web.py
@@ -4,4 +4,4 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("web_app:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("web_app:app", host="127.0.0.1", port=8000, reload=False, workers=2)
